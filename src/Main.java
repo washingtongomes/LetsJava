f150f95e7071f4e6140b9c01193a72b790283a1b
Washington Gomes <wsgomes600@gmail.com>
2022-09-09 19:44:56 -0300

    Aprendendo variaveis em java e numeros primitivos...

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+
+        String nome = "Washington";
+
         System.out.println("Hello world!");
+        System.out.println(nome);
+
     }
 }
