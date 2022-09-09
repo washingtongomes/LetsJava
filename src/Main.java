59423b5403bbc46b3079dc778f02f3f7f8527f1e
Washington Gomes <wsgomes600@gmail.com>
2022-09-09 20:58:09 -0300

    Operadores booleanos e ternario

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,36 @@
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
 
-        String nome = "Washington";
-
-        System.out.println("Hello world!");
+        //variaveis
+        String nome = "Autor: Washington";
         System.out.println(nome);
+        int a;
+        int b =2;
+        a=3;
+        int soma = a+b;
+        int subtracao = a-b;
+        int multiplicacao = a*b;
+        float divisao = (float)a/b;
+
+        System.out.println(soma);
+        System.out.println(subtracao);
+        System.out.println(multiplicacao);
+        System.out.println(divisao);
+
+        //operador booleano
+        boolean fimDeSemana = false;
+        boolean fazendoSol = true;
+        boolean vamosAPraia = fimDeSemana && fazendoSol;
+        System.out.println(vamosAPraia);
+
+        //operador ternario
+        String mensagem = fimDeSemana ? "E fim de semana" : "Não é fim de semana";
+        System.out.println(mensagem);
+
 
     }
+
+
 }
