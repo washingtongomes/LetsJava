9c16811c8e8c6ca7e82c62041c89d7f38c0aaff7
Washington Gomes <wsgomes600@gmail.com>
2022-09-09 21:40:14 -0300

    Altereando projeto

--- src/Main.java
+++ src/Main.java
@@ -29,6 +29,37 @@ public class Main {
         String mensagem = fimDeSemana ? "E fim de semana" : "Não é fim de semana";
         System.out.println(mensagem);
 
+        //Condicionais
+        int nota = 100;
+        String graduacao;
+
+        // A=80,  B=70, C=60, D=0
+        if (nota>=80){
+            graduacao = "A";
+        } else if (nota<80 && nota >=70) {
+            graduacao = "B";
+        } else if (nota<70 && nota >=60) {
+            graduacao = "C";
+        } else if (nota<60 && nota >=0) {
+            graduacao = "D";
+        }else {
+            graduacao = "";
+        }
+        //Estrutura Switch
+
+        switch (graduacao) {
+            case "A":
+            case "B":
+                System.out.println("Aprovado !");
+                break;
+            case "C":
+            case "D":
+                System.out.println("Reprovado");
+                break;
+            default:
+                System.out.println("Graduação invalida");
+
+        }
 
     }
 
