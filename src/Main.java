d9b8eb80ed88dc0c521278c11a16747af6222427
Washington Gomes <wsgomes600@gmail.com>
2022-09-09 23:40:38 -0300

    Estrutura repetição... For

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import javax.swing.text.html.HTMLDocument;
 import java.sql.SQLOutput;
+import java.time.LocalDateTime;
 import java.time.LocalDate;
 import java.time.format.TextStyle;
 import java.util.Locale;
@@ -62,7 +64,6 @@ public class Main {
             default:
                 System.out.println("Graduação invalida");
         }
-        */
 
         // Representando datas
         String nome = "Washington Gomes";
@@ -70,11 +71,28 @@ public class Main {
         //ISO 8601
         LocalDate hoje = LocalDate.now();
         Locale brasil = new Locale("pt" , "BR");
-        System.out.println(hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil ));
+        //System.out.println(hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil ));
+        String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil);
+        String saudacao;
+        LocalDateTime agora = LocalDateTime.now();
+        if(agora.getHour()>= 12 && agora.getHour() < 18){
+            saudacao = "Bom dia ! ! !";
+        }else if (agora.getHour() >= 12 && agora.getHour()<18){
+            saudacao = "Boa tarde ! ! !";
+        }else if(agora.getHour()>18 && agora.getHour()<24){
+            saudacao = "Boa noite ! ! !";
+        }else{
+            saudacao = "Olá";
+        }
+        System.out.printf("Olá, %s. Hoje é %s %s %n", nome, diaSemana, saudacao.toUpperCase() );
 
-
+         */
+            for(int i=1; i<=10; i++){
+                for(int j=1; j<=10; j++)
+                    System.out.println(j + " X " + i + "=" + j * i);
+                    // 1x1=1
+                    // 2x1=2
+            }
 
     }
-
-
 }
